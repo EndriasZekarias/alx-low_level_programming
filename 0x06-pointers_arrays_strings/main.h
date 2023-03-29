37cd952bb7250bfd440186d7a22e55e0e7b025b8
EndriasZekarias <bekizak11@gmail.com>
2023-03-29 09:29:51 -0700

    main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int_putcher(char c);
+char*_strcat(char *dest, char *src)
+	char *_stmcat(char *desk, char *src,int n);
+	char*_stmcpy(char *desk, char *src, int n);
+	int _strcm(char *s1, char (s2);
+	void reverse_array(int*a, int n);
+	char *string_toupper(char *a);
+	char *cap_string(char *a);
+	char *leet(char *a);
+	char *rott13(char *a);
+	void print_number(int n);
+	char *infinite_add(char *n1,char *n2, char *r, int size_r);
+	void print_buffer(char *b, int size);
+
+#endif
